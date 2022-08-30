1c354e6a7f9398c33bb32437b0b6c2dff9a91ecf
berliniixi <steliosevoix@gmail.com>
2022-08-30 16:46:56 +0300

    Controlling a face
    Controlling a face through C# script (eyes , mouth)

--- Assets/BlendController.cs
+++ Assets/BlendController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class BlendController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    SkinnedMeshRenderer _blendShapes;
+
+    [SerializeField] private float eyesReaction; 
+    [SerializeField] private float eyesOpen; // Open and close the eyes
+    [SerializeField] private float mouthReaction; // Open and close the mouth using the mouse 
     void Start()
     {
-        
+        _blendShapes = GetComponent<SkinnedMeshRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        eyesReaction = Input.GetAxis("Vertical") * 100;
+        eyesOpen = Input.GetAxis("Horizontal") * 100;
+        mouthReaction = Input.mousePosition.y;
         
+        _blendShapes.SetBlendShapeWeight(0, eyesReaction);
+        _blendShapes.SetBlendShapeWeight(1, eyesOpen);
+        _blendShapes.SetBlendShapeWeight(2, mouthReaction);
     }
 }
+
